a0251fc0d8d702209ad147fb1e046982ee194350
yesita <hernandez.luna.yesenia.086.14@gmail.com>
2016-04-20 12:27:59 -0500

    piano1.3
    cambio de tamaño

--- js/acciones.js
+++ js/acciones.js
@@ -1,13 +1,21 @@
 //JavaScript
 	
 $(document).ready(function(e){	
+    $('#principal').height($('#page').height());//calcular el alto de la pantalla del dspositivo
 	document.addEventListener("deviceready",function(){
-    $('#beep').tap(function(){
-		navigator.notification.beep(1);
-	});//Tap del beep
-    $('#vibrar').tap(function(){
-		navigator.notification.vibrate(1000);
-	});//Tap del beep	
-	
+		//precarga el sonido
+		audio=window.plugins.LowLatencyAudio;
+		audio.preloadFX('do','audio/DO.mp3',function(){},function(e){alert('Error '+e);});
+        audio.preloadFX('re','audio/RE.mp3',function(){},function(e){alert('Error '+e);});
+		audio.preloadFX('mi','audio/MI.mp3',function(){},function(e){alert('Error '+e);});
+		audio.preloadFX('fa','audio/FA.mp3',function(){},function(e){alert('Error '+e);});
+		audio.preloadFX('sol','audio/SOL.mp3',function(){},function(e){alert('Error '+e);});
+		audio.preloadFX('la','audio/LA.mp3',function(){},function(e){alert('Error '+e);});
+		audio.preloadFX('si','audio/SI.mp3',function(){},function(e){alert('Error '+e);});
+		
+		//reproducir las notas
+		$('.nota').bind('touchstart', function(){$(this).addClass('tocada'); audio.play($(this).attr('id'));});
+		$('.nota').bind('touched',function(){$(this).removeClass('tocada');});
+		
 	},false); //deviceready
 });//ready
